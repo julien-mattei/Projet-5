24f6e1d6ab3e609bba6ca1673eadba9ae0bdcb0e
Julien Mattei <j.mattei@me.com>
2023-11-11 23:16:52 +0100

    Ajout de la page de connexion avec redirection vers la page d'accueil si les infos de login sont correct, ajout du token dans le session storage pour arriver sur la page d'accueil en mode edition. Création de la 1ere modal avec les images de la gallerie'

--- FrontEnd/connexion.js
+++ FrontEnd/connexion.js
@@ -1,3 +1,42 @@
-(async () => {
+const mail = document.getElementById('mail');
+const mdp = document.getElementById('mdp');
+const btn = document.querySelector('input[type=submit]')
+console.log(btn)
 
-})
+const form = document.querySelector('form');
+
+(async() => {
+    form.addEventListener('submit', async(event) => {
+        event.preventDefault()
+    
+        const user = {
+            email : event.target.querySelector('[name=mail]').value,
+            password : event.target.querySelector('[name=mdp]').value
+        }
+        const chargeUtile = JSON.stringify(user)
+    
+        const reponse = await fetch('http://localhost:5678/api/users/login', {
+            method: 'POST',
+            headers: { 
+                'accept': 'application/json',
+                'Content-Type': 'application/json'
+            } ,
+            body: chargeUtile
+        })
+
+        if( reponse.status === 200){
+            const log = await reponse.json()
+            const token = log.token
+            window.sessionStorage.setItem ('token', token)
+            window.location.href='index.html'
+        }
+        else {
+            erreur = document.createElement('span')
+            erreur.innerText = 'Email et Mot de passe incorrect'
+            form.insertBefore(erreur, btn)
+        }
+    });
+})()
+
+
+
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,7 +1,6 @@
 (async() => {
     const reponse = await fetch('http://localhost:5678/api/works')
     const gallery = await reponse.json()
-
     const pf = document.getElementById("portfolio")
     const gall = document.querySelector(".gallery")
 
@@ -18,7 +17,7 @@
     }
     const filters = [...new Set(categories)]
 
-// création de la boucle sur les catégoris pour créer les boutons de filtres
+// création de la boucle sur les catégories pour créer les boutons de filtres
     for(filter = 0; filter < filters.length; filter++){
         const buttons = document.createElement("button")
         buttons.innerText = filters[filter]
@@ -41,7 +40,6 @@
             fig.appendChild(name) 
         }
     }
-
     genererImages(gallery)
 
     for(b = 0; b < btns.length; b++){
@@ -58,6 +56,37 @@
                 document.querySelector(".gallery").innerHTML = ''
                 genererImages(gallery)
             })   
-            }
+        }
+    }
+
+    const jeton = window.sessionStorage.getItem('token')
+    console.log(jeton)
+
+    if (jeton !== null ){
+        const body = document.querySelector('body')
+        const header = document.querySelector('header')
+        console.log(header)
+        const edition = document.createElement('div')
+        edition.classList.add('edition')
+        const lien = document.createElement('a')
+        lien.innerText = 'Mode Édition'
+        edition.appendChild(lien)
+        body.insertBefore(edition, header)
+        const link = document.querySelector('a')
+        link.setAttribute('href', '#modal')
+        console.log(link)
+
+        const gallerie = document.querySelector('.gallerie')
+        for(picture = 0; picture < gallery.length; picture++) {
+            const image = document.createElement("img")
+            image.src = gallery[picture].imageUrl
+            image.alt = gallery[picture].title
+            gallerie.appendChild(image)
+        }
+        link.addEventListener('click', (event) => {
+            event.preventDefault()
+            const target = document.querySelector(event.target.getAttribute('href'))
+            target.style.display = null
+        })
     }
 })()
